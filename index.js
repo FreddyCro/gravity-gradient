f89b7f8cc04c47f39cccf4fc2a561b2a499cee94
FreddyCro <freddy10100@hotmail.com>
2022-01-13 08:53:46 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const config = {
+  positiveColor: ['', ''],
+  negativeColor: '',
+  breakpoint: 1024,
+};
+
+const commonStyle = {
+  'will-chage': 'transform',
+};
+
+// requestAnimationFrame
+
+const gg = document.querySelector('#gg-halo');
